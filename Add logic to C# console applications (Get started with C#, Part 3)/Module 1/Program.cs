3d91fafebcf2889a8259470e6ee7868c0e65ba37
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-10-16 22:23:19 -0300

    Adicionei a pasta contendo os conteúdos até agora realizados da parte 3 do curso
    Modulo 1 e modulo 2 adicionados

---
+++ Add logic to C# console applications (Get started with C#, Part 3)/Module 1/Program.cs
@@ -0,0 +1,90 @@
+﻿//---------------Unit 2---------------------//
+
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
+
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+//---------------Unit 3 ---------------------//
+
+// <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {(saleAmount > 100 ? 100 : 50)}");
+
+//---------------Unit 4 ---------------------//
+// Random coinChance = new Random();
+// int chance = coinChance.Next(1,3);
+// Console.WriteLine($"Result: {(chance == 2 ? "heads" : "tails")}");
+
+//---------------Unit 6 ---------------------//
+string permission = "Manager";
+int level = 00;
+
+if(!permission.Contains("Admin") && !permission.Contains("Manager")){
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+else if(permission.Contains("Manager") && level < 20){
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+else if(permission.Contains("Manager") && level >= 20){
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if(permission.Contains("Admin") && level <= 55){
+        Console.WriteLine("Welcome, Admin user.");
+}
+else{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+// Resposta da Microsoft //
+// string permission = "Admin|Manager";
+// int level = 53;
+
+// if (permission.Contains("Admin"))
+// {
+//     if (level > 55)
+//     {
+//         Console.WriteLine("Welcome, Super Admin user.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Welcome, Admin user.");
+//     }
+// }
+// else if (permission.Contains("Manager"))
+// {
+//     if (level >= 20)
+//     {
+//         Console.WriteLine("Contact an Admin for access.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("You do not have sufficient privileges.");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("You do not have sufficient privileges.");
+// }
---
+++ Add logic to C# console applications (Get started with C#, Part 3)/Module 2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Unit 2 - Code blocks and variable scope 
+
+/*
+bool flag = true;
+int value = 0;
+if (flag)
+{
+    value = 10;
+    Console.WriteLine($"Inside of code block: {value}");
+}
+Console.WriteLine($"Outside of code block: {value}");
+*/
+
+// Unit 3 - Remove code blocks from if statements
+
+/*
+string name = "steve";
+if (name == "bob") 
+    Console.WriteLine("Found Bob");
+else if (name == "steve") 
+    Console.WriteLine("Found Steve");
+else 
+    Console.WriteLine("Found Chuck");
+*/
+
+// Unit 4 - Complete a challenge activity using variable scope
+
+using System.Runtime.InteropServices.Marshalling;
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+        found = true;
+}
+
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
