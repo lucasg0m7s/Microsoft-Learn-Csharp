6b0dac5c01921ee4cf3377a0287f157f9acc78c1
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-10-24 22:01:09 -0300

    module 5 concluído

--- Add logic to C# console applications (Get started with C#, Part 3)/Module 5/Program.cs
+++ Add logic to C# console applications (Get started with C#, Part 3)/Module 5/Program.cs
@@ -137,21 +137,6 @@ do{
 }while(validInput == false);
 
 Console.WriteLine($"Seu valor de entrada ({userInput}) foi aceito.");
-
 */
 
-string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-
-int periodLocation = 0; 
-
-foreach(string myString in myStrings)
-{
-    periodLocation = myString.IndexOf(".");
-    Console.WriteLine(myString.Remove(periodLocation));
-
-    // while(periodLocation != -1){
-    //     Console.WriteLine(myString.Remove(periodLocation));
-    //     periodLocation = myString.IndexOf(".");
-    // }
-}
 
