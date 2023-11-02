397c998b7da9f0e7270f2ef441daa525dbb53c09
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-11-02 14:09:08 -0300

    Unit 5 concluída

--- Add logic to C# console applications (Get started with C#, Part 3)/Module 6/Guided-project/GuidedProject/Starter/Program.cs
+++ Add logic to C# console applications (Get started with C#, Part 3)/Module 6/Guided-project/GuidedProject/Starter/Program.cs
@@ -109,12 +109,12 @@ do
     {
         case "1":
             // List all of our current pet information
-            for(int i = 0; i < maxPets; i++)
+            for (int i = 0; i < maxPets; i++)
             {
-                if(ourAnimals[i, 0] != "ID #: ")
+                if (ourAnimals[i, 0] != "ID #: ")
                 {
                     Console.WriteLine();
-                    for(int j = 0; j < 6; j++)
+                    for (int j = 0; j < 6; j++)
                     {
                         Console.WriteLine(ourAnimals[i, j]);
                     }
@@ -126,9 +126,48 @@ do
 
         case "2":
             // Add a new animal friend to the ourAnimals array
-            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
-            Console.WriteLine("Press the Enter key to continue.");
-            readResult = Console.ReadLine();
+            string anotherPet = "y";
+            int petCount = 0;
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+            }
+
+            if (petCount < maxPets)
+            {
+                Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
+            }
+
+            while (anotherPet == "y" && petCount < maxPets)
+            {
+                // increment petCount (the array is zero-based, so we increment the counter after adding to the array)
+                petCount = petCount + 1;
+
+                if (petCount < maxPets)
+                {
+                    Console.WriteLine("Do you want to enter info for another pet (y/n)");
+                    do
+                    {
+                        readResult = Console.ReadLine();
+                        if (readResult != null)
+                        {
+                            anotherPet = readResult.ToLower();
+                        }
+                    } while (anotherPet != "y" && anotherPet != "n");
+
+                }
+            }
+
+            if (petCount >= maxPets)
+            {
+                Console.WriteLine("We have reached our limit on the number of pets that we can manage.");
+                Console.WriteLine("Press the Enter key to continue.");
+                readResult = Console.ReadLine();
+            }
+
             break;
 
         case "3":
