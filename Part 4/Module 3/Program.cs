70e3609e254ee46f7c8b8ba326a63c521ff97525
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-11-03 22:11:46 -0300

    Module 3 concluído

--- Part 4/Module 3/Program.cs
+++ Part 4/Module 3/Program.cs
@@ -54,7 +54,58 @@ foreach (var pallet in pallets)
 }
 
 Unit 4
-*/
 
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray(); // Transforma em uma matriz de char em que cada elemento da matriz é um caractere da string original
+
+// Array.Reverse(valueArray); // Inverte a ordem dos elementos
+
+// string result = new string(valueArray); // Transforma em uma string novamente
+// Console.WriteLine(result);
+
+// --------------------------------------------------------------
+
+//Join() => Separa cada elemento da matriz com um caractere
 string value = "abc123";
 char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(','); // Cria uma matriz do tipo string, dividindo uma string longa em strings menores
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+
+Unit 5 - Challenge
+
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+char[] pangramChar = pangram.ToCharArray();
+Array.Reverse(pangramChar);
+
+string pangramVersion2 = new string(pangramChar);
+string[] pangramString = pangramVersion2.Split(" ");
+Array.Reverse(pangramString);
+
+string pangramVersion3 = String.Join(" ", pangramString);
+Console.WriteLine(pangramVersion3);
+
+Unit 7 - Challenge
+
+*/
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderIDs = orderStream.Split(',');
+Array.Sort(orderIDs);
+
+for(int i = 0; i < orderIDs.Length; i++){
+    if(orderIDs[i].Length != 4){
+        Console.WriteLine($"{orderIDs[i]} \t- Error");
+    }else{
+        Console.WriteLine(orderIDs[i]);
+    }
+}
