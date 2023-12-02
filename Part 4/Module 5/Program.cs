ec77dbf2d3e9470d376e9fde4ea6a413f206b699
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-02 18:58:56 -0300

    modulo 5 iniciado

---
+++ Part 4/Module 5/Program.cs
@@ -0,0 +1,8 @@
+﻿string message = "What is the value <span>between the tags</span>?";
+
+int openingPosition = message.IndexOf("<span>");
+int closingPosition = message.IndexOf("</span>");
+
+openingPosition += 6;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
